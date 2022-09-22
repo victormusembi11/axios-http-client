797d2392e00c1d883925cb84c939ffc2bba34874
victormusembi11 <victor.musembi11@gmail.com>
2022-09-22 13:52:27 +0300

    feat: GET request w/ axios
    Event listener for get button which calls api and displays output on browser.

---
+++ main.js
@@ -0,0 +1,47 @@
+// GET Request
+function getTodos() {
+    axios({
+        method: 'get',
+        url: 'https://jsonplaceholder.typicode.com/todos',
+        params: {
+            _limit: 5
+        }
+    })
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
+}
+
+document.getElementById('get').addEventListener('click', getTodos);
+
+// Show output in browser
+function showOutput(res) {
+    document.getElementById('res').innerHTML = `
+        <div class="card card-body mb-4">
+        <h5>Status: ${res.status}</h5>
+        </div>
+        <div class="card mt-3">
+        <div class="card-header">
+            Headers
+        </div>
+        <div class="card-body">
+            <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+        </div>
+        </div>
+        <div class="card mt-3">
+        <div class="card-header">
+            Data
+        </div>
+        <div class="card-body">
+            <pre>${JSON.stringify(res.data, null, 2)}</pre>
+        </div>
+        </div>
+        <div class="card mt-3">
+        <div class="card-header">
+            Config
+        </div>
+        <div class="card-body">
+            <pre>${JSON.stringify(res.config, null, 2)}</pre>
+        </div>
+        </div>
+    `;
+}
